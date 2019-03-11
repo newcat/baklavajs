af4052442315eba4253d6d836644ddca1df4ca3e
newcat <freddy.wagner@web.de>
2019-03-11 09:07:54 +0100

    Added function that creates vue instance

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,27 @@
 export * from "./model";
 export { default as Options } from "./options";
 export { NodeBuilder } from "./utility/nodeBuilder";
+
 import Baklava from "./baklava";
+import { Editor } from "./model/editor";
+import Vue from "vue";
+
+export function createBaklava(element: Element): Editor {
+
+    Vue.use(Baklava);
+
+    const editor = new Editor();
+
+    new Vue({
+        render: h => h("baklava-editor", {
+            props: {
+                model: editor
+            }
+        }),
+    }).$mount(element);
+
+    return editor;
+
+}
 
 export default Baklava;
