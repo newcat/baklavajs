aced0d449eea3a577fb85e6564a805b2d1bf759a
newcat <freddy.wagner@web.de>
2019-03-15 09:34:46 +0100

    Fixed some tests

--- tests/unit/specs/components/Editor.spec.ts
+++ tests/unit/specs/components/Editor.spec.ts
@@ -1,39 +1,42 @@
 import Vue from "vue";
 import { expect } from "chai";
-import { createLocalVue, shallowMount, mount } from "@vue/test-utils";
+import { createLocalVue, shallowMount, mount, Wrapper } from "@vue/test-utils";
 import BaklavaJS, { Editor } from "@/index";
 import EditorView from "@/components/Editor.vue";
 import TestNode from "testimpl/TestNode";
 
-const localVue = createLocalVue();
-localVue.use(BaklavaJS);
+Vue.use(BaklavaJS);
 
-function getEditor(shallow = true) {
-    const editor = new Editor();
-    return {
-        editor,
-        view: (shallow ? shallowMount : mount)(EditorView, { propsData: { model: editor }, localVue })
-    };
+function mountEditor(editor?: Editor) {
+    if (!editor) {
+        editor = new Editor();
+    }
+    return mount(EditorView, { propsData: { model: editor } });
 }
 
 describe("EditorView", () => {
 
     it("does render", () => {
-        const { view } = getEditor();
+        const view = mountEditor();
         expect(view.find(".node-editor").exists()).to.be.true;
     });
 
     it("does show nodes", async () => {
-        const { editor, view } = getEditor(false);
+        const editor = new Editor();
         editor.registerNodeType("TestNode", TestNode);
-        const n = (view.vm as any).model.addNode("TestNode");
-        await Vue.nextTick();
-        await Vue.nextTick();
-        await Vue.nextTick();
-        await Vue.nextTick();
-        await Vue.nextTick();
-        console.log(view.html());
+        const n = editor.addNode("TestNode");
+        const view = mountEditor(editor);
         expect(view.find("#" + n!.id).exists()).to.be.true;
+    });
+
+    it("does show connections", async () => {
+        const editor = new Editor();
+        editor.registerNodeType("TestNode", TestNode);
+        const n1 = editor.addNode("TestNode");
+        const n2 = editor.addNode("TestNode");
+        const c = editor.addConnection(n1!.getInterface("Output"), n2!.getInterface("Input"));
+        // const view = mountEditor(editor);
+        // expect(view.find(".connection").exists()).to.be.true;
     });
 
 });
