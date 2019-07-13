67c94e31d6bd14947d06ab4bb41569d0d6967cc7
newcat <freddy.wagner@web.de>
2019-07-14 01:27:24 +0200

    Added baklavajs-full package

---
+++ packages/baklavajs-full/src/index.ts
@@ -0,0 +1,30 @@
+import * as Core from "@baklavajs/core";
+import * as PluginEngine from "@baklavajs/plugin-engine";
+import * as PluginInterfaceTypes from "@baklavajs/plugin-interface-types";
+import * as PluginOptionsVue from "@baklavajs/plugin-options-vue";
+import * as PluginRendererVue from "@baklavajs/plugin-renderer-vue";
+
+import "@baklavajs/plugin-renderer-vue/dist/styles.css";
+
+import Vue from "vue";
+function createBaklava(element: Element): PluginRendererVue.ViewPlugin {
+
+    Vue.use(PluginRendererVue.BaklavaVuePlugin);
+
+    const editor = new Core.Editor();
+    const plugin = new PluginRendererVue.ViewPlugin();
+    editor.use(plugin);
+
+    new Vue({
+        render: (h) => h("baklava-editor", {
+            props: {
+                model: editor
+            }
+        }),
+    }).$mount(element);
+
+    return plugin;
+
+}
+
+export { Core, PluginEngine, PluginInterfaceTypes, PluginOptionsVue, PluginRendererVue, createBaklava };
--- packages/baklavajs-plugin-renderer-vue/src/index.ts
+++ packages/baklavajs-plugin-renderer-vue/src/index.ts
@@ -1,30 +1,3 @@
 export { default as Editor } from "./components/Editor.vue";
 export * from "./baklavaVuePlugin";
 export * from "./viewPlugin";
-
-import Vue from "vue";
-import { ViewPlugin } from "./viewPlugin";
-import { BaklavaVuePlugin } from "./baklavaVuePlugin";
-
-// TODO: Exclude all this into separate package
-import { Editor } from "../../baklavajs-core/src";
-
-export function createBaklava(element: Element): ViewPlugin {
-
-    Vue.use(BaklavaVuePlugin);
-
-    const editor = new Editor();
-    const plugin = new ViewPlugin();
-    editor.use(plugin);
-
-    new Vue({
-        render: (h) => h("baklava-editor", {
-            props: {
-                model: editor
-            }
-        }),
-    }).$mount(element);
-
-    return plugin;
-
-}
