2d58cd12446f8ba11a21b8455e6166e215cd4cf9
newcat <freddy.wagner@web.de>
2020-11-04 00:07:23 +0100

    Fix copy plugin

--- packages/baklavajs-plugin-renderer-vue/webpack.config.js
+++ packages/baklavajs-plugin-renderer-vue/webpack.config.js
@@ -1,20 +1,20 @@
-const path = require('path');
-const merge = require('webpack-merge');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const base = require('../../build/webpack.vue.config');
+const path = require("path");
+const merge = require("webpack-merge");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const base = require("../../build/webpack.vue.config");
 
 module.exports = merge(base, {
     entry: {
-        index: './src/index.ts',
-        styles: './src/styles/all.scss'
+        index: "./src/index.ts",
+        styles: "./src/styles/all.scss"
     },
     plugins: [
-        new CopyWebpackPlugin([
-            { from: "src/styles", to: "styles" }
-        ])
+        new CopyWebpackPlugin({
+            patterns: [{ from: "src/styles", to: "styles" }]
+        })
     ],
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        library: 'BaklavaJSRendererVue'
-    },
+        path: path.resolve(__dirname, "dist"),
+        library: "BaklavaJSRendererVue"
+    }
 });
