67483e54f0ba07f654c2f1162f4d8ce64dc8c8c4
newcat <freddy.wagner@web.de>
2021-06-05 00:18:52 +0200

    Fix linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,15 +2,12 @@
 
 module.exports = {
     root: true,
-    parser: "@typescript-eslint/parser",
+    parser: "vue-eslint-parser",
+    parserOptions: {
+        parser: "@typescript-eslint/parser",
+    },
     plugins: ["@typescript-eslint"],
-    extends: [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "prettier",
-        "prettier/@typescript-eslint",
-        "plugin:vue/vue3-recommended",
-    ],
+    extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier", "plugin:vue/vue3-recommended"],
     rules: {
         "@typescript-eslint/no-non-null-assertion": 0,
         "@typescript-eslint/no-explicit-any": 0,
