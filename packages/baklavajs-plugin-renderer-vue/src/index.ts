f2a10bbae80d1b1f7599a2896691d16761b38904
Frederik Wagner <newcat@users.noreply.github.com>
2021-06-30 22:08:04 +0200

    Add minimap to the list of exported components

--- packages/baklavajs-plugin-renderer-vue/src/index.ts
+++ packages/baklavajs-plugin-renderer-vue/src/index.ts
@@ -8,11 +8,12 @@ import NodeInterface from "./components/node/NodeInterface.vue";
 import NodeOption from "./components/node/NodeOption.vue";
 import ContextMenu from "./components/ContextMenu.vue";
 import Sidebar from "./components/Sidebar.vue";
+import Minimap from "./components/Minimap.vue";
 
 export const Components = {
     Connection, ConnectionWrapper, TemporaryConnection,
     Node, NodeInterface, NodeOption,
-    ContextMenu, Sidebar
+    ContextMenu, Sidebar, Minimap
 };
 
 export * from "./baklavaVuePlugin";
