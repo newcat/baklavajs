d1ac6940e8df07ee3488c27693b3448461cee8da
newcat <freddy.wagner@web.de>
2021-08-24 00:27:22 +0200

    Added missing components to export

--- packages/baklavajs-plugin-renderer-vue/src/index.ts
+++ packages/baklavajs-plugin-renderer-vue/src/index.ts
@@ -7,6 +7,8 @@ import ConnectionWrapper from "./connection/ConnectionWrapper.vue";
 import TemporaryConnection from "./connection/TemporaryConnection.vue";
 import Node from "./node/Node.vue";
 import NodeInterface from "./node/NodeInterface.vue";
+import ContextMenu from "./components/ContextMenu.vue";
+import Minimap from "./components/Minimap.vue";
 import Sidebar from "./components/Sidebar.vue";
 
 export const Components = {
@@ -15,6 +17,8 @@ export const Components = {
     TemporaryConnection,
     Node,
     NodeInterface,
+    ContextMenu,
+    Minimap,
     Sidebar,
 };
 
