d4082016a1ba9dc514ebcba52943b10a686f61c9
newcat <freddy.wagner@web.de>
2022-01-16 04:04:03 +0100

    Fix build

--- packages/renderer-vue/src/sidebar/index.ts
+++ packages/renderer-vue/src/sidebar/index.ts
@@ -3,6 +3,8 @@ import { Graph, NodeInterface } from "@baklavajs/core";
 import { ICommandHandler } from "../commands";
 import { registerOpenSidebarCommand } from "./openSidebar.command";
 
+export { OPEN_SIDEBAR_COMMAND, OpenSidebarCommand } from "./openSidebar.command";
+
 export const displayInSidebar = (intf: NodeInterface, displayInSidebar: boolean) => {
     intf.displayInSidebar = displayInSidebar;
 };
