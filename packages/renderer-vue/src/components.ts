d33458ca5b2f11d4a45902ccee1a985fde7cc3d4
newcat <freddy.wagner@web.de>
2023-06-01 22:27:12 +0200

    Export NodePalette component (#287)

--- packages/renderer-vue/src/components.ts
+++ packages/renderer-vue/src/components.ts
@@ -3,6 +3,7 @@ export { default as ConnectionWrapper } from "./connection/ConnectionWrapper.vue
 export { default as TemporaryConnection } from "./connection/TemporaryConnection.vue";
 export { default as Node } from "./node/Node.vue";
 export { default as NodeInterface } from "./node/NodeInterface.vue";
+export { default as NodePalette } from "./nodepalette/NodePalette.vue";
 export { default as ContextMenu } from "./components/ContextMenu.vue";
 export { default as Minimap } from "./components/Minimap.vue";
 export { default as Sidebar } from "./sidebar/Sidebar.vue";
