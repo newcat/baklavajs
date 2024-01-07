351dd1a98a9bda2cc6da902cdfc0c57db06b8029
a876691666 <928200728@qq.com>
2024-01-07 23:10:36 +0800

    Fix: Refactored the addConnection method in the ConnectionStep class: Now explicitly setting the connection ID to ensure consistency and prevent issues with finding connections by ID.

--- packages/renderer-vue/src/history/connectionStep.ts
+++ packages/renderer-vue/src/history/connectionStep.ts
@@ -43,7 +43,8 @@ export default class ConnectionStep implements IStep {
         if (!fromIntf || !toIntf) {
             return;
         }
-        graph.addConnection(fromIntf, toIntf);
+        const connection = graph.addConnection(fromIntf, toIntf);
+        if (connection) connection.id = this.connectionState!.id;
     }
 
     private removeConnection(graph: Graph) {
