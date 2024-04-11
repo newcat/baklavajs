ec93570d53dbe428e7833291feeb4325b17ec9e1
newcat <freddy.wagner@web.de>
2024-04-12 01:03:25 +0200

    fix(renderer-vue): Type overrides not exported (#396)

--- eslint.config.js
+++ eslint.config.js
@@ -8,7 +8,7 @@ import eslintConfigPrettier from "eslint-config-prettier";
 
 export default tseslint.config(
     {
-        ignores: ["dist", "node_modules"],
+        ignores: ["packages/*/dist", "node_modules"],
     },
     eslint.configs.recommended,
     ...tseslint.configs.recommendedTypeChecked,
--- packages/renderer-vue/src/index.ts
+++ packages/renderer-vue/src/index.ts
@@ -2,6 +2,8 @@
  * @module @baklavajs/renderer-vue
  */
 
+export * from "./overrides";
+
 /** @deprecated use `BaklavaEditor` instead */
 export { default as EditorComponent } from "./editor/Editor.vue";
 export { default as BaklavaEditor } from "./editor/Editor.vue";
