cc09e4209cc3cca38e855a8246fca2658f928674
youzhengwei <yzw@123.>
2026-06-04 09:18:59 +0800

    fix: issue #418

--- packages/engine/test/applyResult.spec.ts
+++ packages/engine/test/applyResult.spec.ts
@@ -45,6 +45,70 @@ describe("applyResult", () => {
         expect(n3.outputs.d.value).toEqual(0);
     });
 
+    it("updates connected input interface values", () => {
+        const e = new Editor();
+        const n1 = new TestNode();
+        const n2 = new TestNode();
+        e.graph.addNode(n1);
+        e.graph.addNode(n2);
+        e.graph.addConnection(n1.outputs.c, n2.inputs.a);
+
+        applyResult(new Map([[n1.id, new Map([["c", 10]])]]), e);
+
+        expect(n1.outputs.c.value).toEqual(10);
+        expect(n2.inputs.a.value).toEqual(10);
+    });
+
+    it("applies connection data transformations to connected input interface values", () => {
+        const e = new Editor();
+        const n1 = new TestNode();
+        const n2 = new TestNode();
+        e.graph.addNode(n1);
+        e.graph.addNode(n2);
+        e.graph.addConnection(n1.outputs.c, n2.inputs.a);
+
+        applyResult(new Map([[n1.id, new Map([["c", 10]])]]), e, {
+            transferData: (value) => String(value),
+        });
+
+        expect(n2.inputs.a.value).toEqual("10");
+    });
+
+    it("doesn't update disconnected input interface values", () => {
+        const e = new Editor();
+        const n1 = new TestNode();
+        const n2 = new TestNode();
+        e.graph.addNode(n1);
+        e.graph.addNode(n2);
+
+        applyResult(new Map([[n1.id, new Map([["c", 10]])]]), e);
+
+        expect(n2.inputs.a.value).toEqual(1);
+    });
+
+    it("updates connected multi-input interface values", () => {
+        const e = new Editor();
+        const n1 = new TestNode();
+        const n2 = new TestNode();
+        const n3 = new TestNode();
+        e.graph.addNode(n1);
+        e.graph.addNode(n2);
+        e.graph.addNode(n3);
+        n3.inputs.a.allowMultipleConnections = true;
+        e.graph.addConnection(n1.outputs.c, n3.inputs.a);
+        e.graph.addConnection(n2.outputs.c, n3.inputs.a);
+
+        applyResult(
+            new Map([
+                [n1.id, new Map([["c", 10]])],
+                [n2.id, new Map([["c", 20]])],
+            ]),
+            e,
+        );
+
+        expect(n3.inputs.a.value).toEqual([10, 20]);
+    });
+
     it("doesn't fail on non-existing ids", () => {
         const e = new Editor();
         const n1 = new TestNode();
